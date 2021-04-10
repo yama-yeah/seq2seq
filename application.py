89122dbbc190448e5c2fb0d135d8f5f51563da76
b1021091 <b1021091@fun.ac.jp>
2021-04-11 07:35:00 +0900

    404

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, abort, make_response
 import os
 import sys
+from werkzeug.exceptions import Forbidden, HTTPException, NotFound, RequestTimeout, Unauthorized
 
 PATH=os.path.abspath('') 
 sys.path.append(PATH)
@@ -10,6 +11,7 @@ import responder
 app = Flask(__name__)
 
 
+
 @app.route("/<key>", methods=["GET"])
 def main(key):
     try:
@@ -17,5 +19,23 @@ def main(key):
     except:
         return jsonify(key+'?')
 
+@app.errorhandler(NotFound)
+def page_not_found_handler(e: HTTPException):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(Unauthorized)
+def unauthorized_handler(e: HTTPException):
+    return render_template('401.html'), 401
+
+
+@app.errorhandler(Forbidden)
+def forbidden_handler(e: HTTPException):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(RequestTimeout)
+def request_timeout_handler(e: HTTPException):
+    return render_template('408.html'), 408
 
 app.run()
